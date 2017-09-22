6c954f761ff4d39cdfb5da1a1819c27bacdb4f23
Will Adams <31661515+WadamsSoyFiesta@users.noreply.github.com>
2017-09-22 15:18:34 -0400

    whoamore :

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -8,6 +8,24 @@ class ListsController < ApplicationController
         flash[:alert] = "There was an error creating your list." flash[:alert] = "There was an error creating your list."
     else
       flash[:alert] = "There was an error creating your list."
+    else
+    else
+    else
+    else
+      else
+        else
+          else
+            else
+              else
+                else
+                  else
+                    else
+                      else
+                        else
+                          else
+                            else
+                              else
+                                else
     end
     redirect_to(list_tasks_url(@list))
   end
